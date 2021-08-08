19bcadaef52659f90dfee17abb85aeedc8df5d05
JoshuaStanfield <joshua.c.stanfield@gmail.com>
2021-08-07 23:53:27 -0600

    git practice

--- StudentList/Model/StudentController.swift
+++ StudentList/Model/StudentController.swift
@@ -10,6 +10,7 @@ import Foundation
 class StudentController {
     //Provides a list of students. This is VERY simplified for learning's sake. More detials to come. The purpose here is to maintain a list of Students or model objects.
     
+    //Singleton
     static var students: [Student] {
         return[
         Student(firstName: "Josh", lastName: "Stanfield", age: 32),
